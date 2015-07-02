be4d426956a73e693f49b562f6cfd00ba8f5acc2
Alan Plum <me@pluma.io>
2015-07-02 19:24:55 +0200

    Satisfied flow.

--- src/router.js
+++ src/router.js
@@ -16,7 +16,7 @@ class RouteNode extends Map<string, RouteNode> {
     this._dynamic = null;
   }
   dynamic(): RouteNode {
-    var node = this._dynamic || new RouteNode();
+    var node: RouteNode = this._dynamic || new RouteNode();
     if (!this._dynamic) this._dynamic = node;
     return node;
   }
@@ -28,27 +28,27 @@ class RouteNode extends Map<string, RouteNode> {
 export class Router {
   _caseInsensitive: boolean;
   _params: Map<string,Param>;
-  _named: Map<string,Route>;
-  _routes: RouteNode;
+  _byName: Map<string,Route>;
+  _root: RouteNode;
   constructor(caseInsensitive: boolean = false) {
     this._caseInsensitive = caseInsensitive;
     this._params = new Map();
-    this._named = new Map();
-    this._routes = new RouteNode();
+    this._byName = new Map();
+    this._root = new RouteNode();
   }
-  param(name: string, resolve: ?(ParamFn | Schema), schema: ?Schema): Router {
+  param(name: string, resolve: any, schema: any): Router {
     if (resolve) {
-      if (!schema && resolve.validate) {
+      if (resolve.validate) {
         schema = resolve;
         resolve = undefined;
       }
-      this._params.set(name, {resolve, schema});
+      this._params.set(name, {resolve, schema})
     }
     return this;
   }
   route(path: string, resolve: RouteFn, name?: string): Router {
     var tokens: Array<string> = path.split('/').filter(Boolean);
-    var node: RouteNode = this._routes;
+    var node: RouteNode = this._root;
     var paramNames: Array<string> = [];
     var route: Route = {name, resolve, paramNames, path: tokens};
     tokens.forEach(token => {
@@ -62,11 +62,11 @@ export class Router {
       }
     });
     node.terminal.push(route);
-    if (name) this._named.set(name, route);
+    if (name) this._byName.set(name, route);
     return this;
   }
   reverse(name: string, params: ?{[name: string]: any}): string {
-    var route = this._named.get(name);
+    var route: ?Route = this._byName.get(name);
     if (!route) throw new Error(`Unknown route: ${name}`);
     return '/' + route.path.map(token => {
       if (token.charAt(0) === ':') {
@@ -78,70 +78,58 @@ export class Router {
     }).join('/');
   }
   resolve(path: string): Promise {
-    var caseInsensitive = this._caseInsensitive;
+    var caseInsensitive: boolean = this._caseInsensitive;
     var paramDefs = this._params;
-    var tokens = path.split('/').filter(Boolean);
-    var matches: Array<{route: Route, params: Array<string>}> = [];
+    var tokens: Array<string> = path.split('/').filter(Boolean);
+    var matches: Array<{route: Route, paramValues: Array<string>}> = [];
 
-    function traverse(route: RouteNode, i: number = 0, params: Array<string> = []) {
+    function traverse(route: RouteNode, i: number = 0, paramValues: Array<string> = []) {
       if (i === tokens.length) {
         if (!route.terminal) return;
-        route.terminal.forEach(r => matches.push({route: r, params}));
+        route.terminal.forEach(r => matches.push({route: r, paramValues}));
         return;
       }
       var token = tokens[i];
       if (caseInsensitive) token = token.toLowerCase();
-      if (route.has(token)) traverse(route.get(token), i + 1, params);
-      if (route.hasDynamic()) traverse(route.dynamic(), i + 1, params.concat(token));
+      if (route.has(token)) traverse(route.get(token), i + 1, paramValues);
+      if (route.hasDynamic()) traverse(route.dynamic(), i + 1, paramValues.concat(token));
     }
 
     function next(err) {
       if (err) return Promise.reject(err);
       if (!matches.length) return Promise.reject(404);
+      var promisedParams: {[t: string]: Promise} = {};
+      var resolvedParams: {[t: string]: any} = {};
+      var {paramValues, route} = matches.shift();
 
-      var promisedParams = {};
-      var resolvedParams = {};
-      var {
-        params: paramValues,
-        route: {
-          resolve: resolveRoute,
-          paramNames
-        }
-      } = matches.shift();
+      function paramValueToPromise(value: any, i: number): Promise {
+          var name = route.paramNames[i];
+          var promise = promisedParam(value, name)
+          .then(value => {
+            resolvedParams[name] = value;
+          });
+          promisedParams[name] = promise;
+          return promise;
+      }
 
-      var paramPromises = paramValues.map((value, i) => {
-        var name = paramNames[i];
-        var promise = promisedParam(value, name)
-        .then(value => {
-          resolvedParams[name] = value;
-        });
-        promisedParams[name] = promise;
-        return promise;
-      });
-
-      function promisedParam(value, name) {
+      function promisedParam(value: any, name: string): Promise {
         if (!paramDefs.has(name)) return Promise.resolve(value);
-        var {
-          resolve: resolveParam,
-          schema
-        } = paramDefs.get(name);
-
-        if (schema) {
-          var result = schema.validate(value);
+        var param = paramDefs.get(name);
+        if (param.schema) {
+          var result = param.schema.validate(value);
           if (result.error) return Promise.reject(result.error);
           value = result.value;
         }
-
         return Promise.resolve(value)
-        .then(v => resolveParam ? resolveParam(v, promisedParams) : v);
+        .then(v => param.resolve ? param.resolve(v, promisedParams) : v);
       }
 
-      return Promise.all(paramPromises)
-      .then(() => resolveRoute(resolvedParams))
+      return Promise.all(paramValues.map(paramValueToPromise))
+      .then(() => route.resolve(resolvedParams))
       .then(undefined, err => (!err || err.ignore) ? next() : next(err));
     }
 
-    traverse(this._routes);
+    traverse(this._root);
 
     return next();
   }
